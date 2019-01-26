1a4f53de4d098b66c9fca76e35241e0fe44501d9
Nenad <nstokic@gmail.com>
2019-01-26 02:15:21 +0100

    finished posting contact with first and last name to mailchimp list

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// ff5527efa08eda1758d6f080ee23a657-us19   api key
+// 71bcb56172  list id
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
@@ -19,7 +21,39 @@ app.post("/", function(req, res){
   var lastName = req.body.lastname;
   var email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  var data = {
+      members: [
+        {
+          email_address: email,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+      url: "https://us19.api.mailchimp.com/3.0/lists/71bcb56172",
+      method: "POST",
+      headers: {
+        "Authorization" : "Nenad ff5527efa08eda1758d6f080ee23a657-us19"
+      },
+      body: jsonData,
+
+  };
+
+  request(options, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+
 });
 
 app.listen(3000, function(){
